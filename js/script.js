b0298200551d8d63d8a887c25e7d9f05da331b9e
Viktor Zmachynski <vikvzm@mail.ru>
2024-09-10 16:29:34 +0300

    plase a card using a class, default element

--- js/script.js
+++ js/script.js
@@ -167,12 +167,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Создаем карточки при помощи класса
     class MenuCard {
-        constructor(scr, alt, title, descr, price, parentSelector) {
+        constructor(scr, alt, title, descr, price, parentSelector, ...classes) {
             this.scr = scr;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 3;
             this.changeToBYN();
@@ -184,17 +185,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
+            //назначаем элемент по умолчании
+            if (this.classes.length ===0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
             element.innerHTML = `
-            <div class="menu__item">
-                    <img src=${this.scr} alt=${this.alt}>
-                    <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">${this.descr}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> руб./день</div>
-                    </div>
+<!--            <div class="menu__item">-->
+            <img src=${this.scr} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> руб./день</div>
             </div>
+<!--            </div>-->
             `;
             this.parent.append(element);
         }

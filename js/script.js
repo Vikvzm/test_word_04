4ff0a38df7b50c68ccb6af5a65b8043c9cfabb4e
Viktor Zmachynski <vikvzm@mail.ru>
2024-09-30 16:00:19 +0300

    creating a slide v2 carousel

--- js/script.js
+++ js/script.js
@@ -450,46 +450,117 @@ document.addEventListener('DOMContentLoaded', () => {
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
-        current = document.querySelector('#current');
+        current = document.querySelector('#current'),
+        slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+        slidesField = document.querySelector('.offer__slider-inner'),
+        width = window.getComputedStyle(slidesWrapper).width;
 
-    let  slideIndex = 1;
+    let slideIndex = 1;
+    let offset = 0;
 
-    showSledes(slideIndex);
 
+
+
+    //слайд вариант 1
+
+    //
+    // showSledes(slideIndex);
+    //
+    // if (slides.length < 10) {
+    //     total.textContent = `0${slides.length}`;
+    // } else {
+    //     total.textContent = slides.length;
+    // }
+    //
+    // function showSledes(n) {
+    //     if (n > slides.length) {
+    //         slideIndex = 1;
+    //     }
+    //     if (n < 1) {
+    //         slideIndex = slides.length;
+    //     }
+    //     //скрываем все слайды
+    //     slides.forEach(item => item.style.display = 'none');
+    //     //выводим нужный слайд
+    //     slides[slideIndex - 1].style.display = 'block';
+    //
+    //     if (slideIndex < 10) {
+    //         current.textContent = `0${slideIndex}`;
+    //     } else {
+    //         current.textContent = slideIndex;
+    //     }
+    // }
+    // function plusSlides(n) {
+    //     showSledes(slideIndex += n);
+    // }
+    //
+    // prev.addEventListener('click', () => {
+    //     plusSlides(-1);
+    // });
+    //
+    // next.addEventListener('click', () => {
+    //     plusSlides(1);
+    // });
+
+    //слайд вариант2 карусель
     if (slides.length < 10) {
         total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
     } else {
         total.textContent = slides.length;
+        current.textContent = slideIndex;
     }
 
-    function showSledes(n) {
-        if (n > slides.length) {
-            slideIndex = 1;
-        }
-        if (n < 1) {
-            slideIndex = slides.length;
-        }
-        //скрываем все слайды
-        slides.forEach(item => item.style.display = 'none');
-        //выводим нужный слайд
-        slides[slideIndex - 1].style.display = 'block';
+    slidesField.style.width = `${100 * slides.length}%`;
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
 
+    slidesWrapper.style.overflow = 'hidden';
+
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+
+    next.addEventListener('click', () => {
+        if(offset == +width.slice(0,width.length - 2) * (slides.length -1)) {
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length-2);
+        }
+
+        slidesField.style.transform = `translatex(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
         if (slideIndex < 10) {
             current.textContent = `0${slideIndex}`;
         } else {
             current.textContent = slideIndex;
         }
-    }
-    function plusSlides(n) {
-        showSledes(slideIndex += n);
-    }
-
-    prev.addEventListener('click', () => {
-        plusSlides(-1);
     });
 
-    next.addEventListener('click', () => {
-        plusSlides(1);
+    prev.addEventListener('click', () => {
+        if(offset == 0) {
+            offset = +width.slice(0,width.length - 2) * (slides.length -1);
+        } else {
+            offset -= +width.slice(0, width.length-2);
+        }
+
+        slidesField.style.transform = `translatex(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+        if (slideIndex < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
     });
 
 });
